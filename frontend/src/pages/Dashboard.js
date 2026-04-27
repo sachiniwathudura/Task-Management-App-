dffac8e77c630847df8783deee6b5684191f6c07
sachiniwathudura <sachiniwathudura@gmail.com>
2026-04-27 06:30:09 +0530

    add logout button

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -8,46 +8,61 @@ import {
   updateTodo
 } from "../store/todosSlice";
 
+import { useNavigate } from "react-router-dom";
 
 import TodoForm from "../components/TodoForm";
 import TodoItem from "../components/TodoItem";
 
 export default function Dashboard() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { items, loading, adding, error } = useSelector((state) => state.todos);
 
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
 
-return (
-  <div className="page">
-    <div className="container">
-      <h2 className="heading">📄 TODOs</h2>
+  function handleLogout() {
+    localStorage.removeItem("token");
+    navigate("/"); // redirect to login
+  }
 
-      {error && <p className="error">{error}</p>}
+  return (
+    <div className="page">
+      <div className="container">
 
-      <TodoForm
-        loading={adding}
-        onAdd={(data) => dispatch(addTodo(data))}
-      />
+        <div className="top-bar">
+          <button className="logout-btn" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
 
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        items.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            onToggle={(id) => dispatch(toggleTodo(id))}
-            onDelete={(id) => dispatch(deleteTodo(id))}
-            onUpdate={(id, payload) =>
-              dispatch(updateTodo({ id, payload }))
-            }
-          />
-        ))
-      )}
+        <h2 className="heading">📄 TODOs</h2>
+
+        {error && <p className="error">{error}</p>}
+
+        <TodoForm
+          loading={adding}
+          onAdd={(data) => dispatch(addTodo(data))}
+        />
+
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          items.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onToggle={(id) => dispatch(toggleTodo(id))}
+              onDelete={(id) => dispatch(deleteTodo(id))}
+              onUpdate={(id, payload) =>
+                dispatch(updateTodo({ id, payload }))
+              }
+            />
+          ))
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
 }
